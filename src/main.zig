2d51b0261d7185592da8c9f457337d171f406e66
ckysmrn <dezavue3@gmail.com>
2025-06-27 06:28:42 +0700

    Don't ask

--- src/main.zig
+++ src/main.zig
@@ -9,14 +9,12 @@ pub fn main() !void {
     print("Hello world\n", .{});
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer {
-        const d = gpa.deinit();
-        if(d == .leak) std.testing.expect(false) catch @panic("Cannot free gpa.\n");
+        const _ = gpa.deinit();
     }
     const allocator = gpa.allocator();
     const text = try allocator.dupe(u8, "text");
     defer allocator.free(text);
     print("{s}\n", .{text});
-
 }
 
 
