2b89179f54eac5231385b88ac9965f6127d20732
ckysmrn <dezavue3@gmail.com>
2025-06-27 06:02:53 +0700

    Don't ask

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,16 @@
 
 test "lsp" {
     const std = @import("std");
-    std.debug.print("Hello world", .{});
+    const print = std.debug.print;
+    print("Hello world\n", .{});
+    const gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer gpa.deinit();
+    const allocator = gpa.allocator();
+    const text = try allocator.alloc(u8, "text");
+    defer allocator.free(text);
+    print("{}\n", .{text});
+
+
 }
 
 pub fn main() !void {
