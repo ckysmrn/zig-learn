1ebd09c64594ff4fccbaffbe4d86a35913238878
ckysmrn <ckysmrn@gmail.com>
2025-06-30 00:52:12 +0700

    Don't ask

--- parser/src/main.zig
+++ parser/src/main.zig
@@ -13,7 +13,7 @@ pub fn main() !void {
     const DebugAllocator = std.heap.DebugAllocator(.{});
     var dbg_allocator: ?DebugAllocator = null;
     const allocator: std.mem.Allocator = if(@import("builtin").mode == .Debug) {
-        dbg_allocator = std.heap.DebugAllocator.init(gpa);
+        dbg_allocator = DebugAllocator.init(gpa);
         dbg_allocator.?.allocator();
     } else {
         gpa;
